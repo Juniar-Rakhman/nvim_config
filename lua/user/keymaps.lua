09787655c0db85bb0bd7144a6861c3e92880ac01
juniar <juniar.rahman@rithm.nl>
2022-08-27 02:42:32 +0700

    modified more stuff

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,7 @@ local keymap = vim.keymap.set
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
 keymap("n", "<C-i>", "<C-i>", opts)
 
@@ -21,18 +22,28 @@ keymap("n", "<C-i>", "<C-i>", opts)
 --   term_mode = "t",
 --   command_mode = "c",
 
+-- easier home and end 
+keymap("", "<S-h>", "^", opts)
+keymap("", "<S-l>", "$", opts)
+
+-- Delete without yanking
+keymap("n", "d", "\"_d", opts)
+keymap("v", "d", "\"_d", opts)
+
+-- replace currently selected text with default register without yanking it
+keymap("v", "p", "\"_dP", opts)
+
 -- Normal --
 -- Better window navigation
-keymap("n", "<m-h>", "<C-w>h", opts)
-keymap("n", "<m-j>", "<C-w>j", opts)
-keymap("n", "<m-k>", "<C-w>k", opts)
-keymap("n", "<m-l>", "<C-w>l", opts)
-keymap("n", "<m-tab>", "<c-6>", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Tabs --
-keymap("n", "<m-t>", ":tabnew %<cr>", opts)
-keymap("n", "<m-y>", ":tabclose<cr>", opts)
-keymap("n", "<m-\\>", ":tabonly<cr>", opts)
+-- keymap("n", "<m-t>", ":tabnew %<cr>", opts)
+-- keymap("n", "<m-y>", ":tabclose<cr>", opts)
+-- keymap("n", "<m-\\>", ":tabonly<cr>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -41,10 +52,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
--- keymap("n", "<S-l>", ":bnext<CR>", opts)
--- keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
--- keymap("n", "<RightMouse>", ":Alpha<CR>", opts)
+keymap("n", "<A-l>", ":bnext<CR>", opts)
+keymap("n", "<A-h>", ":bprevious<CR>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -57,4 +66,7 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- NvimTree
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>")
 
